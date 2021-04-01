571c62f732c44f4cfcd38fbba3a7446b30d2bb06
Thomas Pisula <tompisula@gmail.com>
2021-04-01 14:50:38 +0200

    update ci

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -3,3 +3,7 @@
 sudo docker build -t arcade-blazor-client -f docker/arcade-blazor-client.Dockerfile .
 sudo docker tag arcade-blazor-client 10.101.153.109:5000/arcade-blazor-client
 sudo docker push 10.101.153.109:5000/arcade-blazor-client
+
+sudo docker build -t arcade-server -f docker/arcade-server.Dockerfile .
+sudo docker tag arcade-server 10.101.153.109:5000/arcade-server
+sudo docker push 10.101.153.109:5000/arcade-server
