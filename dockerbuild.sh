cad024e2aea1b25d6b3fb2d232e067aac30344d6
Thomas Pisula <tompisula@gmail.com>
2021-04-02 13:38:17 +0200

    found it

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -6,7 +6,7 @@ sudo docker build -t arcade-server-gol -f docker/arcade-server-gol.Dockerfile .
 
 sudo docker tag arcade-blazor-client 10.101.153.109:5000/arcade-blazor-client
 sudo docker tag arcade-server 10.101.153.109:5000/arcade-server
-sudo docker tag arcade-server 10.101.153.109:5000/arcade-server-gol
+sudo docker tag arcade-server-gol 10.101.153.109:5000/arcade-server-gol
 
 sudo docker push 10.101.153.109:5000/arcade-blazor-client
 sudo docker push 10.101.153.109:5000/arcade-server
