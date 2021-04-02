6b8558bc7e97d010fb63c5ec5e5223ee15560f4a
Thomas Pisula <tompisula@gmail.com>
2021-04-02 13:33:49 +0200

    update build

--- dockerbuild.sh
+++ dockerbuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo docker build -t arcade-blazor-client -f docker/arcade-blazor-client.Dockerfile .
-sudo docker tag arcade-blazor-client 10.101.153.109:5000/arcade-blazor-client
-sudo docker push 10.101.153.109:5000/arcade-blazor-client
-
 sudo docker build -t arcade-server -f docker/arcade-server.Dockerfile .
-sudo docker tag arcade-server 10.101.153.109:5000/arcade-server
-sudo docker push 10.101.153.109:5000/arcade-server
-
 sudo docker build -t arcade-server-gol -f docker/arcade-server-gol.Dockerfile .
+
+sudo docker tag arcade-blazor-client 10.101.153.109:5000/arcade-blazor-client
+sudo docker tag arcade-server 10.101.153.109:5000/arcade-server
 sudo docker tag arcade-server 10.101.153.109:5000/arcade-server-gol
+
+sudo docker push 10.101.153.109:5000/arcade-blazor-client
+sudo docker push 10.101.153.109:5000/arcade-server
 sudo docker push 10.101.153.109:5000/arcade-server-gol
