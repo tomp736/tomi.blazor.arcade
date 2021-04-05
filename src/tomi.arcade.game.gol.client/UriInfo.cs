70dafa147a61e73f61133eca2ceed32db0bc6723
Thomas Pisula <tompisula@gmail.com>
2021-04-05 11:16:57 +0200

    create appconfig, refactor gol client parts out of main client

---
+++ src/tomi.arcade.game.gol.client/UriInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tomi.arcade.game.gol.client
+{
+    public class UriInfo
+    {
+        public string Host { get; set; }
+        public string Port { get; set; }
+        public string Protocol { get; set; }
+
+        public Uri ToUri()
+        {
+            return new Uri($"{Protocol}://{Host}:{Port}");
+        }
+    }
+}
