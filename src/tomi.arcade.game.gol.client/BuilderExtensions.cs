071df74c093a9ba1ccfc6899712c2ee478b71006
Thomas Pisula <tompisula@gmail.com>
2021-04-05 11:17:49 +0200

    create appconfig, refactor gol client parts out of main client (#8)

---
+++ src/tomi.arcade.game.gol.client/BuilderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
+
+namespace tomi.arcade.game.gol.client
+{
+    public static class BuilderExtensions
+    {
+        public static void AddGameOfLifeServiceClient(this WebAssemblyHostBuilder builder)
+        {
+            builder.Services.AddGrpcClient<protos.GameOfLifeService.GameOfLifeServiceClient>("gameoflife", (provider, options) =>
+            {
+                var config = provider.GetService<IConfiguration>();
+                UriInfo settings = config.GetSection("arcadeServer").Get<UriInfo>();
+
+                options.Address = settings.ToUri();
+            })
+            .ConfigureChannel((provider, options) =>
+            {
+                // configure channel to use grpc-web handler
+                options.HttpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler());
+            });
+        }
+    }
+}
